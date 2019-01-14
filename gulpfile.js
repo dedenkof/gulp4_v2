d39e6709c169a57098ce8cef196f971fee12a8a0
Volodymyr Dedenko <dedenkof@ukr.net>
2019-01-14 18:09:33 +0200

    Add minification css and rename files and fixed sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,49 @@ const gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     plumber = require('gulp-plumber'),
-    gcmq = require('gulp-group-css-media-queries');
+    gcmq = require('gulp-group-css-media-queries'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename');
+
 
 
 function preproc(){
     return gulp.src('./src/sass/**/*.scss')
         .pipe(plumber())
-        .pipe(sass())// компилим sass
-        .pipe(gcmq())
         .pipe(sourcemaps.init())
+        .pipe(sass())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['> 0.1%'],
             cascade: false
         }))
-        .pipe(sourcemaps.write())
+        .pipe(gcmq())
+        .pipe(rename({suffix: '.min'})) // Добавляем в название файла суфикс .min
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('build/css'))
 
 }
 
 // style
-function styles(){
+function styles() {
     return gulp.src('./src/css/**/*.css')
-        .pipe(gcmq())
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(autoprefixer({
             browsers: ['> 0.1%'],
             cascade: false
         }))
+        .pipe(gcmq())
         .pipe(concat('general.css'))
+        .pipe(rename({suffix: '.min'})) // Добавляем в название файла суфикс .min
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('build/css'))
-
 }
 
 // js
@@ -45,6 +58,9 @@ function scripts(){
 
 }
 
-gulp.task('styles', styles);
 gulp.task('scripts', scripts);
+gulp.task('styles', styles);
 gulp.task('preproc', preproc);
+
+
+
