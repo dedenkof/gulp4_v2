aa35f72eb92954a5695daa5396d7355bcb7417b6
Volodymyr Dedenko <dedenkof@ukr.net>
2019-01-14 12:11:53 +0200

    Add media queries group

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,15 @@ const gulp = require('gulp'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    plumber = require('gulp-plumber');
+    plumber = require('gulp-plumber'),
+    gcmq = require('gulp-group-css-media-queries');
+
 
 function preproc(){
     return gulp.src('./src/sass/**/*.scss')
         .pipe(plumber())
         .pipe(sass())// компилим sass
+        .pipe(gcmq())
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
@@ -18,36 +21,30 @@ function preproc(){
             cascade: false
         }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest('build/css'))
+
 }
 
 // style
 function styles(){
     return gulp.src('./src/css/**/*.css')
+        .pipe(gcmq())
         .pipe(autoprefixer({
             browsers: ['> 0.1%'],
             cascade: false
         }))
         .pipe(concat('general.css'))
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest('build/css'))
+
 }
 
 // js
 function scripts(){
     return gulp.src('./src/js/**/*.js')
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest('build/js'))
+
 }
 
 gulp.task('styles', styles);
 gulp.task('scripts', scripts);
 gulp.task('preproc', preproc);
-
-
-// Merge all css files in one
-/*
- gulp.task('css', function(){
- return gulp.src('app/css/!*.css')
- .pipe(concatCss("all-pluging.css"))
- .pipe(minifyCss())
- .pipe(gulp.dest('dist/css'));
- });*/
